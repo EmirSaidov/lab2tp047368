b81cd1e8e70899ecb704732164a1c3ce0cbcc76e
Emir-Saidov <emir@reldyn.co>
2021-04-30 10:52:50 +0800

    Add project files.

---
+++ lab2tp047368/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace lab2tp047368.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
